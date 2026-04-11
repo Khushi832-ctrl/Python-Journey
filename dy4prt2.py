97278f8b0ee5a78b73fc59eda86ee14bb29c67e2
Khushi <khushigit120@gmail.com>
2026-04-11 21:16:42 +0530

    Python indexing operation with syntax and error examples

---
+++ dy4prt2.py
@@ -0,0 +1,18 @@
+#operations on str:Indexing operation,Slicing operation
+#Indexing operation: the process of obtaining single value by passing index value from str object. 
+# syntax: strobj[index] here strobj is an object of <class,'str'> and here index can be either positive or negative.
+#if we enter valid positive or negative index then we get corresponding index value else we get error.
+s="python"
+print(s,type(s))
+#output: <class 'str'>
+
+print(s[0])
+#output:p
+
+print(s[5])
+#output:n
+
+print(s[10])
+#output:indexerror
+
+#....to be continued
